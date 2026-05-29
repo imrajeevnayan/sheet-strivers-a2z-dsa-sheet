d830a06762347ed03612a4d1392c53db68304718
CodeSync <codesync@users.noreply.github.com>
2026-05-29 21:13:27 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/all/ENG/solution.py
@@ -0,0 +1,24 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/159/
+# Language: Python
+# Difficulty: Unknown
+# Topics: Binary Search, Test Data Commit Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-05-29T15:43:04.268Z
+
+public·class·Solution·{
+····public·int·findMin(int[]·nums)·{
+········int·start=0,end=nums.length-1,ans=nums[0];
+········while(start<=end){
+············int·mid=start+(end-start)/2;
+············if(nums[mid]>=nums[0])start=mid+1;
+············else{
+················ans=nums[mid];
+················end=mid-1;
+············}
+········}
+········return·ans;
+····}
+}
