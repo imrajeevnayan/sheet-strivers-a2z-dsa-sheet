de2083fae302b27707e5ccc89004c29a1a7a0d39
CodeSync <codesync@users.noreply.github.com>
2026-06-01 00:36:28 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/stackmonotonic-stack/ENG/solution.java
@@ -0,0 +1,13 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/1201/
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: StackMonotonic Stack, Stack, Monotonic Stack, Google, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-05-31T19:06:28.679Z
+ */
+
+public·class·Solution·{
