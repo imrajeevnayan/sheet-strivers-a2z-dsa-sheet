82092cb41e1f3b679f1f702146aaf6b5c1353b81
CodeSync <codesync@users.noreply.github.com>
2026-06-01 00:36:30 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stackmonotonic-stack/ENG/solution.java
+++ CodeSync/LintCode/unknown/stackmonotonic-stack/ENG/solution.java
@@ -7,7 +7,25 @@
  * Topics: StackMonotonic Stack, Stack, Monotonic Stack, Google, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-05-31T19:06:28.679Z
+ * Synced: 2026-05-31T19:06:30.189Z
  */
 
 public·class·Solution·{
+····public·int[]·nextGreaterElements(int[]·nums)·{
+········int·n·=·nums.length;
+········Stack<Integer>·st·=·new·Stack<>();
+········int[]·res·=·new·int[n];
+········Arrays.fill(res,·-1);
+········for·(int·i·=·0;·i·<·2·*·n;·i++)·{
+············int·num·=·nums[i·%·n];
+············while·(!st.isEmpty()·&&·nums[st.peek()]·<·num)·{
+················res[st.pop()]·=·num;
+············}
+············if·(i·<·n)·{
+················st.push(i);
+············}
+········}
+
+········return·res;
+····}
+}
