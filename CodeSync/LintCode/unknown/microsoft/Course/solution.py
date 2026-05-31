aee9980d77bfb53fcf64bf0e4286191b3726a4c4
CodeSync <codesync@users.noreply.github.com>
2026-06-01 01:05:37 +0530

    Add LintCode solution: Course

--- CodeSync/LintCode/unknown/microsoft/Course/solution.py
+++ CodeSync/LintCode/unknown/microsoft/Course/solution.py
@@ -3,10 +3,10 @@
 # URL: https://www.lintcode.com/problem/365/description?showListFe=false&page=1&problemTypeId=4&pageSize=50
 # Language: Python
 # Difficulty: Unknown
-# Topics: Binary, Microsoft, Test Data Test Output
+# Topics: Binary, Microsoft, Test Data Commit Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-05-31T19:35:31.836Z
+# Synced: 2026-05-31T19:35:34.354Z
 
 public·class·Solution·{
 ····public·int·countOnes(int·num)·{
