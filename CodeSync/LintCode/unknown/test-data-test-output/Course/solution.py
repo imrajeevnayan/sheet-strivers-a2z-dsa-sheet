6b9186a667dc800d3d9af2a974372fd49dff41f3
CodeSync <codesync@users.noreply.github.com>
2026-06-01 00:37:42 +0530

    Add LintCode solution: Course

---
+++ CodeSync/LintCode/unknown/test-data-test-output/Course/solution.py
@@ -0,0 +1,15 @@
+# Platform: LintCode
+# Problem: Course
+# URL: https://www.lintcode.com/problem/365/description?showListFe=false&page=1&problemTypeId=4&pageSize=50
+# Language: Python
+# Difficulty: Unknown
+# Topics: Binary, Microsoft, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-05-31T19:07:35.401Z
+
+public·class·Solution·{
+····public·int·countOnes(int·num)·{
+·······
+····}
+}
