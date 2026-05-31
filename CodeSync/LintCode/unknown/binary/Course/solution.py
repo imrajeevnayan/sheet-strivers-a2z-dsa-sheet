6c045e84aa764948e941a73b3b2aca84aae05587
CodeSync <codesync@users.noreply.github.com>
2026-06-01 00:37:24 +0530

    Add LintCode solution: Course

---
+++ CodeSync/LintCode/unknown/binary/Course/solution.py
@@ -0,0 +1,19 @@
+# Platform: LintCode
+# Problem: Course
+# URL: https://www.lintcode.com/problem/365/description?showListFe=false&page=1&problemTypeId=4&pageSize=50
+# Language: Python
+# Difficulty: Unknown
+# Topics: Binary, Microsoft, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-05-31T19:07:24.398Z
+
+public·class·Solution·{
+····/**
+·····*·@param·num:·An·integer
+·····*·@return:·An·integer,·the·number·of·ones·in·num
+·····*/
+····public·int·countOnes(int·num)·{
+········//·write·your·code·here
+····}
+}
