a56304d440d56bf8432ab79723cd1a911318c9b3
CodeSync <codesync@users.noreply.github.com>
2026-06-02 23:47:00 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stack/ENG/solution.java
+++ CodeSync/LintCode/unknown/stack/ENG/solution.java
@@ -1,27 +1,42 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/363/
+ * URL: https://www.lintcode.com/problem/495/?showListFe=true&page=1&problemTypeId=4&tagIds=361&ordering=level&pageSize=50
  * Language: Java
  * Difficulty: Unknown
- * Topics: Opposite Direction Two Pointers, Stack, Two Pointers, Monotonic Stack, Apple, Twitter, Airbnb, Amazon
+ * Topics: Stack, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-02T07:11:07.981Z
+ * Synced: 2026-06-02T18:16:59.577Z
  */
 
-public·class·Solution·{
-····public·int·trapRainWater(int[]·heights)·{
-········int·ans=0;
-········Deque<Integer>st=new·ArrayDeque<>();
-········for(int·i=0;i<heights.length;i++){
-············while(!st.isEmpty()&&·heights[i]>heights[st.peek()]){
-········}
-················int·top=st.pop();
-············}
-················if(st.isEmpty())break;
-··········st.push(i);············
+public·class·Stack·{
+····/*
+·····*·@param·x:·An·integer
+·····*·@return:·nothing
+·····*/
+····public·void·push(int·x)·{
+········//·write·your·code·here
+····}
 
-················int·dist=i-st.peek()-1;
-················int·boundHeight=Math.min(heights[i],heights[st.peek()])-heights[top];
-················ans+=dist·*·boundHeight;
+····/*
+·····*·@return:·nothing
+·····*/
+····public·void·pop()·{
+········//·write·your·code·here
+····}
+
+····/*
+·····*·@return:·An·integer
+·····*/
+····public·int·top()·{
+········//·write·your·code·here
+····}
+
+····/*
+·····*·@return:·True·if·the·stack·is·empty
+·····*/
+····public·boolean·isEmpty()·{
+········//·write·your·code·here
+····}
+}
