356f574feafce0f7d8fa14de4c999b1d4c18660d
CodeSync <codesync@users.noreply.github.com>
2026-06-02 15:37:56 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/all/ENG/solution.py
+++ CodeSync/LintCode/all/ENG/solution.py
@@ -3,14 +3,20 @@
 # URL: https://www.lintcode.com/problem/3820/
 # Language: Python
 # Difficulty: Unknown
-# Topics: StackString, Stack, String, Test Data Test Output
+# Topics: StackString, Stack, String, Test Data Commit Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-02T10:05:48.961Z
+# Synced: 2026-06-02T10:07:40.046Z
 
 public·class·Solution·{
 ····public·String·removeDuplicates(String·s)·{
 ····Stack<Character>st=new·Stack<>();
+····for(char·c:s.toCharArray()){
+········if(!st.isEmpty()&&·st.peek()==c)st.pop();
 ····}
-····for(char·c:s.toCharArray())
+····}
+········else·st.push(c);
+····StringBuilder·sb=new·StringBuilder();
+····for(char·c:st)sb.append(c);
+····return·sb.toString();
 }
