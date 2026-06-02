bfbd1ee7f3b001d8d7ed0a88f2913eb44dce647c
CodeSync <codesync@users.noreply.github.com>
2026-06-02 15:35:48 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stackstring/ENG/solution.py
+++ CodeSync/LintCode/unknown/stackstring/ENG/solution.py
@@ -6,11 +6,11 @@
 # Topics: StackString, Stack, String, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-02T10:05:43.694Z
+# Synced: 2026-06-02T10:05:48.961Z
 
 public·class·Solution·{
 ····public·String·removeDuplicates(String·s)·{
 ····Stack<Character>st=new·Stack<>();
 ····}
-····for(char·c:s.to)
+····for(char·c:s.toCharArray())
 }
