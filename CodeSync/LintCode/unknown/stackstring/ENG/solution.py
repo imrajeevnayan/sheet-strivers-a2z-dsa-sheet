3c155d5452a54f64d5676137d03a7a0603e6ff66
CodeSync <codesync@users.noreply.github.com>
2026-06-02 15:35:07 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stackstring/ENG/solution.py
+++ CodeSync/LintCode/unknown/stackstring/ENG/solution.py
@@ -6,12 +6,16 @@
 # Topics: StackString, Stack, String, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-02T10:04:52.326Z
+# Synced: 2026-06-02T10:05:08.429Z
 
-Input:
-s = "abbaca"
-Output:
-"ca"
-Explanation:
-First delete two adjacent letters b, then delete two adjacent letters a
-"abbaca" -> "aaca" -> "ca"
+public·class·Solution·{
+····public·String·removeDuplicates(String·s)·{
+········StringBuilder·sb=new·StringBuilder();
+········for(char·c:s.toCharArray()){
+············int·len=sb.length();
+············if(len·>0·&&·sb.charAt(len-1)==c)sb.deleteCharAt(len-1);
+············else·sb.append(c);
+········}
+········return·sb.toString();
+····}
+}
