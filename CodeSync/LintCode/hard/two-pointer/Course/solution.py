b4e1380faeac4f634f2aaa0f0390ed87f81f2c0d
CodeSync <codesync@users.noreply.github.com>
2026-06-02 13:27:13 +0530

    Add LintCode solution: Course

--- CodeSync/LintCode/hard/two-pointer/Course/solution.py
+++ CodeSync/LintCode/hard/two-pointer/Course/solution.py
@@ -6,20 +6,20 @@
 # Topics: Opposite Direction Two Pointers, Stack, Two Pointers, Monotonic Stack, Apple, Twitter, Airbnb, Amazon
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-02T07:14:28.587Z
+# Synced: 2026-06-02T07:57:13.816Z
 
-public·class·Solution·{
-····public·int·trapRainWater(int[]·height)·{
-········if·(height·==·null·||·height.length·==·0)·return·0;
-····
-····int·left·=·0,·right·=·height.length·-·1;
-····int·leftMax·=·0,·rightMax·=·0;
-····int·water·=·0;
-····
-····while·(left·<·right)·{
-········if·(height[left]·<·height[right])·{
-············if·(height[left]·>=·leftMax)·{
-················leftMax·=·height[left];
-············}·else·{
 ················water·+=·leftMax·-·height[left];
 ············}
+············left++;
+········}·else·{
+············if·(height[right]·>=·rightMax)·{
+················rightMax·=·height[right];
+············}·else·{
+················water·+=·rightMax·-·height[right];
+············}
+············right--;
+········}
+····}
+····return·water;
+····}
+}
