697c67b97fff76a50f989f9a649497e5ee1e13dd
CodeSync <codesync@users.noreply.github.com>
2026-06-02 12:36:36 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/two-pointer/ENG/solution.java
+++ CodeSync/LintCode/unknown/two-pointer/ENG/solution.java
@@ -7,7 +7,7 @@
  * Topics: Opposite Direction Two Pointers, Stack, Two Pointers, Monotonic Stack, Apple, Twitter, Airbnb, Amazon
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-02T07:06:24.776Z
+ * Synced: 2026-06-02T07:06:36.582Z
  */
 
 public·class·Solution·{
@@ -15,7 +15,7 @@ public·class·Solution·{
 ········int·ans=0;
 ········Deque<Integer>st=new·ArrayDeque<>();
 ········for(int·i=0;i<heights.length;i++){
-············while(!st.isEmpty)
+············while(!st.isEmpty())
 ········}
 ····}
 }
