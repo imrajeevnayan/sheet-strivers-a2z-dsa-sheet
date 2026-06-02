2c59839b570f032ad3a72d6387d0a0cdc047ae46
CodeSync <codesync@users.noreply.github.com>
2026-06-02 12:35:09 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -7,12 +7,13 @@
  * Topics: Opposite Direction Two Pointers, Stack, Two Pointers, Monotonic Stack, Apple, Twitter, Airbnb, Amazon
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-02T07:04:21.397Z
+ * Synced: 2026-06-02T07:05:01.663Z
  */
 
 public·class·Solution·{
 ····public·int·trapRainWater(int[]·heights)·{
 ····}
 ········int·ans=0;
-········Deque<Integer>st=new·
+········Deque<Integer>st=new·ArrayDeque<>();
+········for(int·i=0;i<heights.length;i)
 }
