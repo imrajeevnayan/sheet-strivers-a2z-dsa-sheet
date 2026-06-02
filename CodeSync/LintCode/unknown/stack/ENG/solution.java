bfe08109bcf9f721e40965883fd40672a8afc090
CodeSync <codesync@users.noreply.github.com>
2026-06-02 23:49:02 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stack/ENG/solution.java
+++ CodeSync/LintCode/unknown/stack/ENG/solution.java
@@ -7,21 +7,21 @@
  * Topics: Stack, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-02T18:19:00.020Z
+ * Synced: 2026-06-02T18:19:02.208Z
  */
 
 public·class·Stack·{
-
-····int[]·arr;
-····int·top;
-····int·capacity;
-
-····public·Stack(int·size)·{
-········capacity·=·size;
-········arr·=·new·int[size];
-········top·=·-1;
+····/*
+·····*·@param·x:·An·integer
+·····*·@return:·nothing
+·····*/
+····public·void·push(int·x)·{
+········//·write·your·code·here
 ····}
 
-····public·void·push(int·x)·{
-········if·(top·==·capacity·-·1)·return;
-········arr[++top]·=·x;
+····/*
+·····*·@return:·nothing
+·····*/
+····public·void·pop()·{
+········//·write·your·code·here
+····}
