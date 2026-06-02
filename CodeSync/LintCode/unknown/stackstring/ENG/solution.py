0d75597e9042a31b2b93e25de92f3c3a0dc4828a
CodeSync <codesync@users.noreply.github.com>
2026-06-02 15:34:52 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/stackstring/ENG/solution.py
@@ -0,0 +1,17 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/3820/
+# Language: Python
+# Difficulty: Unknown
+# Topics: StackString, Stack, String, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-06-02T10:04:52.326Z
+
+Input:
+s = "abbaca"
+Output:
+"ca"
+Explanation:
+First delete two adjacent letters b, then delete two adjacent letters a
+"abbaca" -> "aaca" -> "ca"
