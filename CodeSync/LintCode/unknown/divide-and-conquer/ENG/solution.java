d13f717b9e4ed2f8ba6781b9874b4cf24b0b6443
CodeSync <codesync@users.noreply.github.com>
2026-06-02 23:45:13 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/divide-and-conquer/ENG/solution.java
+++ CodeSync/LintCode/unknown/divide-and-conquer/ENG/solution.java
@@ -7,14 +7,10 @@
  * Topics: Divide and Conquer, Google, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-02T18:15:09.230Z
+ * Synced: 2026-06-02T18:15:12.578Z
  */
 
 public·class·Solution·{
-·····*·@param·n:·an·integer
-····/**
-·····*·@return:·return·any·beautiful·array·A
-·····*/
 ····public·int[]·beautifulArray(int·n)·{
 ········//·write·your·code·here.
 ····}
