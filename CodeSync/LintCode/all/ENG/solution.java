ed367ab61e52fc88b9547c29efe58adb5fa7cfdb
CodeSync <codesync@users.noreply.github.com>
2026-06-02 23:47:13 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/all/ENG/solution.java
+++ CodeSync/LintCode/all/ENG/solution.java
@@ -1,17 +1,42 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/1710/
+ * URL: https://www.lintcode.com/problem/495/?showListFe=true&page=1&problemTypeId=4&tagIds=361&ordering=level&pageSize=50
  * Language: Java
  * Difficulty: Unknown
- * Topics: Divide and Conquer, Google, Test Data Test Output
+ * Topics: Stack, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-02T18:15:16.400Z
+ * Synced: 2026-06-02T18:16:59.577Z
  */
 
-public·class·Solution·{
-····public·int[]·beautifulArray(int·n)·{
-········
+public·class·Stack·{
+····/*
+·····*·@param·x:·An·integer
+·····*·@return:·nothing
+·····*/
+····public·void·push(int·x)·{
+········//·write·your·code·here
+····}
+
+····/*
+·····*·@return:·nothing
+·····*/
+····public·void·pop()·{
+········//·write·your·code·here
+····}
+
+····/*
+·····*·@return:·An·integer
+·····*/
+····public·int·top()·{
+········//·write·your·code·here
+····}
+
+····/*
+·····*·@return:·True·if·the·stack·is·empty
+·····*/
+····public·boolean·isEmpty()·{
+········//·write·your·code·here
 ····}
 }
