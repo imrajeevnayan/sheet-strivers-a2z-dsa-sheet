68707077337813c3c377a31df0c6a6d8512b51d1
CodeSync <codesync@users.noreply.github.com>
2026-06-02 23:50:24 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/please-contact-us-via-problem-correction/ENG/solution.py
@@ -0,0 +1,19 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/2506/?showListFe=true&page=1&problemTypeId=4&tagIds=361&ordering=level&pageSize=50
+# Language: Python
+# Difficulty: Unknown
+# Topics: PRE, please contact us via "Problem Correction", StackString, Stack, String, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-06-02T18:20:18.307Z
+
+public·class·Solution·{
+····/**
+·····*·@param·s:·A·string·with·lowercase·letters·and·parentheses
+·····*·@return:·A·string·which·has·been·removed·invalid·parentheses
+·····*/
+····public·String·removeParentheses(String·s)·{
+········//·write·your·code·here.
+····}
+}
