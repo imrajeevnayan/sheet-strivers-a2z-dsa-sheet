4a27956a30062a8ed23fe48ee7a92ce646336061
CodeSync <codesync@users.noreply.github.com>
2026-06-02 23:44:34 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/google/ENG/solution.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/1710/
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Divide and Conquer, Google, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-02T18:14:29.982Z
+ */
+
+public·class·Solution·{
+····public·int[]·beautifulArray(int·n)·{
+······
+····}
+}
