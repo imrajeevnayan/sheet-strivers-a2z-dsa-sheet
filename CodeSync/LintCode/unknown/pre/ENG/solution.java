bc1bcd7659d4423db97f57669d2b532bf2cb980b
CodeSync <codesync@users.noreply.github.com>
2026-06-03 14:07:49 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/pre/ENG/solution.java
+++ CodeSync/LintCode/unknown/pre/ENG/solution.java
@@ -7,7 +7,7 @@
  * Topics: PRE, please contact us via "Problem Correction", StackString, Stack, String, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-03T08:37:47.075Z
+ * Synced: 2026-06-03T08:37:48.577Z
  */
 
 ············}·else·if·(c·==·')')·{
@@ -21,7 +21,7 @@
 ········boolean[]·remove·=·new·boolean[n];
 ····public·String·removeParentheses(String·s)·{
 ········int·n·=·s.length();
-
 public·class·Solution·{
 ················if·(!stack.isEmpty())·{
 ····················stack.pop();·//·matched
+················}·else·{
