3efaacb09b1e30936c34c7be598492a7098d98ae
CodeSync <codesync@users.noreply.github.com>
2026-06-03 14:06:00 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/stackstring/ENG/solution.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/2506
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: PRE, please contact us via "Problem Correction", StackString, Stack, String, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-03T08:35:48.728Z
+ */
+
+public·class·Solution·{
+····/**
+·····*·@param·s:·A·string·with·lowercase·letters·and·parentheses
+·····*·@return:·A·string·which·has·been·removed·invalid·parentheses
+·····*/
+····public·String·removeParentheses(String·s)·{
+········//·write·your·code·here.
+····}
+}
