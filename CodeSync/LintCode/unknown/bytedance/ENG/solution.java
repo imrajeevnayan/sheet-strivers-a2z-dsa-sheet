913c49164d45bf0b93340a5ba6028fc8dfeb4021
CodeSync <codesync@users.noreply.github.com>
2026-06-03 12:18:25 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/bytedance/ENG/solution.java
@@ -0,0 +1,22 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/1527/
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Simulation, ByteDance, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-03T06:48:21.359Z
+ */
+
+public·class·Solution·{
+····/**
+·····*·@param·cards:·A·list·of·cards.
+·····*·@return:·A·list·of·feasible·solution.
+·····*··········we·will·sort·your·return·value·in·output
+·····*/
+····public·List<Integer>·getTheNumber(int[]·cards)·{
+········//·write·your·code·here
+····}
+}
