b997454f8757a2cbd4d93522e4b0dcf7848807ca
CodeSync <codesync@users.noreply.github.com>
2026-06-03 12:59:22 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/2506-remove-the-invalid-parentheses-easy/ENG/solution.java
@@ -0,0 +1,21 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/263
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Stack, 2506 Remove the Invalid Parentheses Easy, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-03T07:29:18.344Z
+ */
+
+public·class·Solution·{
+····/**
+·····*·@param·string:·A·string
+·····*·@return:·whether·the·string·is·valid·
+·····*/
+····public·boolean·matchParentheses(String·string)·{
+········//·write·your·code·here
+····}
+}
