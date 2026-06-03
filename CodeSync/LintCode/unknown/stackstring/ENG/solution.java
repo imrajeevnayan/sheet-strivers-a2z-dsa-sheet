3c0af0a6bfe9f886ea09cc6508f3bef10265beb6
CodeSync <codesync@users.noreply.github.com>
2026-06-03 14:07:54 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stackstring/ENG/solution.java
+++ CodeSync/LintCode/unknown/stackstring/ENG/solution.java
@@ -7,15 +7,21 @@
  * Topics: PRE, please contact us via "Problem Correction", StackString, Stack, String, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-03T08:37:05.050Z
+ * Synced: 2026-06-03T08:37:47.075Z
  */
 
-public·class·Solution·{
+············}·else·if·(c·==·')')·{
+················stack.push(i);
+············if·(c·==·'(')·{
+
+············char·c·=·s.charAt(i);
+········for·(int·i·=·0;·i·<·n;·i++)·{
+
+········Deque<Integer>·stack·=·new·ArrayDeque<>();
+········boolean[]·remove·=·new·boolean[n];
 ····public·String·removeParentheses(String·s)·{
-········StringBuilder·sb·=·new·StringBuilder();
-········for·(char·c·:·s.toCharArray())·{
-············if·(c·!=·'('·&&·c·!=·')')··sb.append(c);
-········}
-········return·sb.toString();
-····}
-}
+········int·n·=·s.length();
+
+public·class·Solution·{
+················if·(!stack.isEmpty())·{
+····················stack.pop();·//·matched
