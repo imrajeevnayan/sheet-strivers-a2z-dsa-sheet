9d1dc22a3536059d9240d78e1f225beb12c147b6
CodeSync <codesync@users.noreply.github.com>
2026-06-03 14:06:25 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/please-contact-us-via-problem-correction/ENG/solution.java
+++ CodeSync/LintCode/unknown/please-contact-us-via-problem-correction/ENG/solution.java
@@ -7,15 +7,20 @@
  * Topics: PRE, please contact us via "Problem Correction", StackString, Stack, String, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-03T08:35:55.650Z
+ * Synced: 2026-06-03T08:36:20.723Z
  */
 
 public·class·Solution·{
-····/**
-·····*·@param·s:·A·string·with·lowercase·letters·and·parentheses
-·····*·@return:·A·string·which·has·been·removed·invalid·parentheses
-·····*/
+
 ····public·String·removeParentheses(String·s)·{
-········//·write·your·code·here.
+········StringBuilder·sb·=·new·StringBuilder();
+
+········for·(char·c·:·s.toCharArray())·{
+············if·(c·!=·'('·&&·c·!=·')')·{
+················sb.append(c);
+············}
+········}
+
+········return·sb.toString();
 ····}
 }
