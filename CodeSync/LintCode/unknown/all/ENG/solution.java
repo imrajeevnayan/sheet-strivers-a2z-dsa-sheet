a78f3ca0d0e046db16105e1114ddb689a1fbbb36
CodeSync <codesync@users.noreply.github.com>
2026-06-03 12:18:33 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -1,27 +1,22 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/495/?showListFe=true&page=1&problemTypeId=4&tagIds=361&ordering=level&pageSize=50
+ * URL: https://www.lintcode.com/problem/1527/
  * Language: Java
  * Difficulty: Unknown
- * Topics: Stack, Test Data Test Output
+ * Topics: Simulation, ByteDance, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-02T18:19:02.208Z
+ * Synced: 2026-06-03T06:48:21.359Z
  */
 
-public·class·Stack·{
-····/*
-·····*·@param·x:·An·integer
-·····*·@return:·nothing
+public·class·Solution·{
+····/**
+·····*·@param·cards:·A·list·of·cards.
+·····*·@return:·A·list·of·feasible·solution.
+·····*··········we·will·sort·your·return·value·in·output
 ·····*/
-····public·void·push(int·x)·{
+····public·List<Integer>·getTheNumber(int[]·cards)·{
 ········//·write·your·code·here
 ····}
-
-····/*
-·····*·@return:·nothing
-·····*/
-····public·void·pop()·{
-········//·write·your·code·here
-····}
+}
