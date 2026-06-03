af6d3fdf0d711400729733bbf354d003d4d0fbc1
CodeSync <codesync@users.noreply.github.com>
2026-06-03 13:02:31 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/easy/test-data-commit-output/ENG/solution.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/263
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Stack, 2506 Remove the Invalid Parentheses Easy, Test Data Commit Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-03T07:32:22.032Z
+ */
+
+public·class·Solution·{
+····public·boolean·matchParentheses(String·string)·{
+········Stack<Character>·st·=·new·Stack<>();
+········for·(char·ch·:·string.toCharArray())·{
+············if·(ch·==·'('·||·ch·==·'{'·||·ch·==·'[')·st.push(ch);
+············else·{
+················if·(st.isEmpty())·return·false;
+················char·top·=·st.pop();
+················if·((ch·==·')'·&&·top·!=·'(')·||
+····················(ch·==·'}'·&&·top·!=·'{')·||
+····················(ch·==·']'·&&·top·!=·'['))·{
+····················return·false;
+················}
+············}
+········}
