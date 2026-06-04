f2f6d6b40bdfb104a775483ef5ac85fe81733863
CodeSync <codesync@users.noreply.github.com>
2026-06-04 17:03:35 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/monotonic-stack/ENG/solution.py
@@ -0,0 +1,22 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/1740/
+# Language: Python
+# Difficulty: Unknown
+# Topics: StackMonotonic Stack, Stack, Monotonic Stack, Microsoft, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-06-04T11:33:27.156Z
+
+public·class·StockSpanner·{
+····public·StockSpanner()·{
+········
+····}
+····/**
+·····*·@param·price:·
+·····*·@return:·int
+·····*/
+····public·int·next(int·price)·{
+········//·Write·your·code·here.
+····}
+}
