6eeb5e109d9739de4cded37f7dedd72707097e6c
CodeSync <codesync@users.noreply.github.com>
2026-06-04 22:43:54 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/stackgreedy/ENG/solution.py
@@ -0,0 +1,19 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/1721/
+# Language: Python
+# Difficulty: Unknown
+# Topics: StackGreedy, Stack, Greedy, Facebook, 2506 Remove the Invalid Parentheses Easy, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-06-04T17:13:53.886Z
+
+public·class·Solution·{
+····/**
+·····*·@param·s:·the·given·string
+·····*·@return:·the·minimum·number·of·parentheses·we·must·add
+·····*/
+····public·int·minAddToMakeValid(String·s)·{
+········//·Write·your·code·here
+····}
+}
