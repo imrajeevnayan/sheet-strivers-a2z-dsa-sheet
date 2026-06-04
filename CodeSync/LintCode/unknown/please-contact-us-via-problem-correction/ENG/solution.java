b8e9588bb52cd0b49a735548b40c8e8e549b877c
CodeSync <codesync@users.noreply.github.com>
2026-06-04 15:27:19 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/please-contact-us-via-problem-correction/ENG/solution.java
+++ CodeSync/LintCode/unknown/please-contact-us-via-problem-correction/ENG/solution.java
@@ -7,15 +7,9 @@
  * Topics: PRE, please contact us via "Problem Correction", Stack, Google, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-04T09:57:10.718Z
+ * Synced: 2026-06-04T09:57:15.852Z
  */
 
-public·class·Solution·{
-····public·String·removeDuplicateLetters(String·s)·{
-·····HashMap<Character,Integer>map=new·HashMap<>();
-·····Stack<Character>st=new·Stack<>();
-·····int·freq[]=new·int[26];
-·····for(char·c:s.toCharArray())·freq[c-'a']++;
 ·····for(char·c:s.toCharArray){
 ·········freq[c-'a']--;
 ·········if(map.contains(c))·continue;
