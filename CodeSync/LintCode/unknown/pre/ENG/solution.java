9985a9237b330b62e4bf0d64b186a01835147e82
CodeSync <codesync@users.noreply.github.com>
2026-06-04 15:27:07 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/pre/ENG/solution.java
+++ CodeSync/LintCode/unknown/pre/ENG/solution.java
@@ -7,7 +7,7 @@
  * Topics: PRE, please contact us via "Problem Correction", Stack, Google, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-04T09:57:05.429Z
+ * Synced: 2026-06-04T09:57:07.253Z
  */
 
 public·class·Solution·{
@@ -18,8 +18,16 @@ public·class·Solution·{
 ·····for(char·c:s.toCharArray())·freq[c-'a']++;
 ·····for(char·c:s.toCharArray){
 ·········freq[c-'a']--;
+·········if(map.contains(c))·continue;
+·········while·(!st.isEmpty()·&&·st.peek()·>·c·&&·freq[st.peek()·-·'a']·>·0)·{
+················map.remove(st.pop());
+············}
+············st.push(c);
+············map.add(c);
+········}
+········StringBuilder·sb·=·new·StringBuilder();
+········for·(char·c·:·st)·sb.append(c);
+········return·sb.toString();
 ·····}
 ····}
-·········if(map.contains(c))·continue;
-·········
 }
