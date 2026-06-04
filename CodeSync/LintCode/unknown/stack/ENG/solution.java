157eec6394661e014a589c351393ce581a63b754
CodeSync <codesync@users.noreply.github.com>
2026-06-04 15:28:04 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stack/ENG/solution.java
+++ CodeSync/LintCode/unknown/stack/ENG/solution.java
@@ -4,20 +4,24 @@
  * URL: https://www.lintcode.com/problem/834/
  * Language: Java
  * Difficulty: Unknown
- * Topics: PRE, please contact us via "Problem Correction", Stack, Google, Test Data Test Output
+ * Topics: PRE, please contact us via "Problem Correction", Stack, Google, Test Data Commit Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-04T09:56:15.837Z
+ * Synced: 2026-06-04T09:57:56.440Z
  */
 
-public·class·Solution·{
-····public·String·removeDuplicateLetters(String·s)·{
-·····HashMap<Character,Integer>map=new·HashMap<>();
-·····Stack<Character>st=new·Stack<>();
-·····int·freq[]=new·int[26];
-·····for(char·c:s.toCharArray())·freq[c-'a']++;
-·····for(char·c:s.toCharArray){
-·········freq[c-'a']--;
-·····}
+········for·(char·c·:·s.toCharArray())·{
+············freq[c·-·'a']--;
+············if·(map.contains(c))·continue;
+············while·(!st.isEmpty()·&&·st.peek()·>·c·&&·freq[st.peek()·-·'a']·>·0)·{
+················map.remove(st.pop());
+············}
+············st.push(c);
+············map.add(c);
+········}
+········StringBuilder·sb·=·new·StringBuilder();
+········for·(char·c·:·st)·sb.append(c);
+········return·sb.toString();
+·····
 ····}
 }
