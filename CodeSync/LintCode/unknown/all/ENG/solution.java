825531c74b6d00e70c3957e07b4d7037f87e6766
CodeSync <codesync@users.noreply.github.com>
2026-06-04 15:26:10 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -7,15 +7,17 @@
  * Topics: PRE, please contact us via "Problem Correction", Stack, Google, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-04T09:55:12.939Z
+ * Synced: 2026-06-04T09:55:58.147Z
  */
 
 public·class·Solution·{
 ····public·String·removeDuplicateLetters(String·s)·{
 ·····HashMap<Character,Integer>map=new·HashMap<>();
-····}
 ·····Stack<Character>st=new·Stack<>();
 ·····int·freq[]=new·int[26];
 ·····for(char·c:s.toCharArray())·freq[c-'a']++;
-·····
+·····for(char·c:s.toCharArray){
+·········freq[c-'a']--;
+·····}
+····}
 }
