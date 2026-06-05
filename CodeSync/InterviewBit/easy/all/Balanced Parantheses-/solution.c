d1bef6ca3616aa9e201f2936fd0373b094347c72
CodeSync <codesync@users.noreply.github.com>
2026-06-05 09:30:53 +0530

    Add InterviewBit solution: Balanced Parantheses!

---
+++ CodeSync/InterviewBit/easy/all/Balanced Parantheses-/solution.c
@@ -0,0 +1,20 @@
+/*
+ * Platform: InterviewBit
+ * Problem: Balanced Parantheses!
+ * URL: https://www.interviewbit.com/problems/balanced-parantheses/
+ * Language: C
+ * Difficulty: Easy
+ * Topics: Programming, Stacks And Queues, Description, Discussion, Submissions, Hints, MAXSPPROD 88 Minutes Medium Asked in:, Hotel Service 51 Minutes Medium Asked in:
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-05T04:00:44.092Z
+ */
+
+public class Solution {
+    public int solve(String A) {
+    }
+        Stack<Character>st=new Stack<>();
+        for(char c:A.toCharArray()){
+            if(c=='(')
+        }
+}
