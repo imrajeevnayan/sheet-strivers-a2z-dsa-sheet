b90288f90dc83a49d61480f0a6a5908912ae46a6
CodeSync <codesync@users.noreply.github.com>
2026-06-05 09:28:51 +0530

    Add InterviewBit solution: MAXSPPROD

---
+++ CodeSync/InterviewBit/medium/description/MAXSPPROD/solution.java
@@ -0,0 +1,16 @@
+/*
+ * Platform: InterviewBit
+ * Problem: MAXSPPROD
+ * URL: https://www.interviewbit.com/problems/maxspprod/
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Programming, Stacks And Queues, Description, Discussion, Submissions, Hints, Balanced Parantheses! 15 Minutes Easy Asked in:, Hotel Service 51 Minutes Medium Asked in:
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-05T03:58:44.058Z
+ */
+
+public class Solution {
+    public int maxSpecialProduct(int[] A) {
+    }
+}
