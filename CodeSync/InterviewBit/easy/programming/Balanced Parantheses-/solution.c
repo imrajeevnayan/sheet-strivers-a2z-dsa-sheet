0ce0ef79fa256955a51063ad1365b7b51e07143d
CodeSync <codesync@users.noreply.github.com>
2026-06-05 09:34:09 +0530

    Add InterviewBit solution: Balanced Parantheses!

--- CodeSync/InterviewBit/easy/programming/Balanced Parantheses-/solution.c
+++ CodeSync/InterviewBit/easy/programming/Balanced Parantheses-/solution.c
@@ -7,7 +7,7 @@
  * Topics: Programming, Stacks And Queues, Description, Discussion, Submissions, Hints, MAXSPPROD 88 Minutes Medium Asked in:, Hotel Service 51 Minutes Medium Asked in:
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-05T04:04:07.089Z
+ * Synced: 2026-06-05T04:04:09.104Z
  */
 
 public class Solution {
