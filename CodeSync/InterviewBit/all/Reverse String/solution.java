a08e9c73e470f91a6e652fa3aa8d459616936985
CodeSync <codesync@users.noreply.github.com>
2026-06-05 09:27:35 +0530

    Add InterviewBit solution: Reverse String

--- CodeSync/InterviewBit/all/Reverse String/solution.java
+++ CodeSync/InterviewBit/all/Reverse String/solution.java
@@ -7,7 +7,7 @@
  * Topics: Programming, Stacks And Queues, Description, Discussion, Submissions, Hints, MAXSPPROD 88 Minutes Medium Asked in:, Balanced Parantheses! 15 Minutes Easy Asked in:
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-05T03:56:38.919Z
+ * Synced: 2026-06-05T03:57:22.986Z
  */
 
 public class Solution {
@@ -15,4 +15,8 @@ public class Solution {
     }
         Stack<Character>st=new Stack<>();
         for(char c:A.toCharArray())st.push(c);
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
 }
