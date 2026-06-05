ad72af1ed4c5134ba6761161dd3b7ddcdcc10503
CodeSync <codesync@users.noreply.github.com>
2026-06-05 09:27:54 +0530

    Add InterviewBit solution: Reverse String

--- CodeSync/InterviewBit/all/Reverse String/solution.java
+++ CodeSync/InterviewBit/all/Reverse String/solution.java
@@ -7,7 +7,7 @@
  * Topics: Programming, Stacks And Queues, Description, Discussion, Submissions, Hints, MAXSPPROD 88 Minutes Medium Asked in:, Balanced Parantheses! 15 Minutes Easy Asked in:
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-05T03:57:22.986Z
+ * Synced: 2026-06-05T03:57:40.920Z
  */
 
 public class Solution {
@@ -20,3 +20,4 @@ public class Solution {
             sb.append(st.pop());
         }
 }
+        return sb.toString();
