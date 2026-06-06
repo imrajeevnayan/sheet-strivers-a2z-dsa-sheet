fe328d4d0ff20cfe3167294958b8352b6b5b1b0b
CodeSync <codesync@users.noreply.github.com>
2026-06-06 13:10:12 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/palantir-technologies-palantir-airbnb/ENG/solution.java
@@ -0,0 +1,22 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/1319/
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Hash Table, Palantir Technologies Palantir Airbnb, Palantir Technologies, Palantir, Airbnb, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-06T07:40:05.243Z
+ */
+
+public·class·Solution·{
+····/**
+·····*·@param·nums:·the·given·array
+·····*·@param·k:·the·given·number
+·····*·@return:··whether·there·are·two·distinct·indices·i·and·j·in·the·array·such·that·nums[i]·=·nums[j]·and·the·absolute·difference·between·i·and·j·is·at·most·k
+·····*/
+····public·boolean·containsNearbyDuplicate(int[]·nums,·int·k)·{
+········//·Write·your·code·here
+····}
+}
