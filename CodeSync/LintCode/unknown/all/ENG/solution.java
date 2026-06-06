41b3d1dddef0a5e548412e2b365529d9ac63b131
CodeSync <codesync@users.noreply.github.com>
2026-06-06 13:10:23 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -1,27 +1,22 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/834/
+ * URL: https://www.lintcode.com/problem/1319/
  * Language: Java
  * Difficulty: Unknown
- * Topics: PRE, please contact us via "Problem Correction", Stack, Google, Test Data Commit Output
+ * Topics: Hash Table, Palantir Technologies Palantir Airbnb, Palantir Technologies, Palantir, Airbnb, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-04T09:57:56.440Z
+ * Synced: 2026-06-06T07:40:05.243Z
  */
 
-········for·(char·c·:·s.toCharArray())·{
-············freq[c·-·'a']--;
-············if·(map.contains(c))·continue;
-············while·(!st.isEmpty()·&&·st.peek()·>·c·&&·freq[st.peek()·-·'a']·>·0)·{
-················map.remove(st.pop());
-············}
-············st.push(c);
-············map.add(c);
-········}
-········StringBuilder·sb·=·new·StringBuilder();
-········for·(char·c·:·st)·sb.append(c);
-········return·sb.toString();
-·····
+public·class·Solution·{
+····/**
+·····*·@param·nums:·the·given·array
+·····*·@param·k:·the·given·number
+·····*·@return:··whether·there·are·two·distinct·indices·i·and·j·in·the·array·such·that·nums[i]·=·nums[j]·and·the·absolute·difference·between·i·and·j·is·at·most·k
+·····*/
+····public·boolean·containsNearbyDuplicate(int[]·nums,·int·k)·{
+········//·Write·your·code·here
 ····}
 }
