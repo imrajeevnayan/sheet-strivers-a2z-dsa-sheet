9f0829fe6314a23302116571ba13a90ba1f471ca
CodeSync <codesync@users.noreply.github.com>
2026-06-06 13:11:24 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/palantir-technologie/ENG/solution.java
+++ CodeSync/LintCode/unknown/palantir-technologie/ENG/solution.java
@@ -7,12 +7,14 @@
  * Topics: Hash Table, Palantir Technologies Palantir Airbnb, Palantir Technologies, Palantir, Airbnb, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-06T07:40:40.922Z
+ * Synced: 2026-06-06T07:41:13.252Z
  */
 
 public·class·Solution·{
 ····public·boolean·containsNearbyDuplicate(int[]·nums,·int·k)·{
 ······Map<Integer,Integer>map=new·HashMap<>();
+······for(int·i=0;i<nums.length;i++){
+··········
+······}
 ····}
-······for(int·i=0;)
 }
