78f19d84e62834ff4a9b7fef9d0f63ca3e2c04aa
CodeSync <codesync@users.noreply.github.com>
2026-06-06 13:13:00 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/all/ENG/solution.java
+++ CodeSync/LintCode/all/ENG/solution.java
@@ -7,7 +7,7 @@
  * Topics: Hash Table, Palantir Technologies Palantir Airbnb, Palantir Technologies, Palantir, Airbnb, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-06T07:42:15.707Z
+ * Synced: 2026-06-06T07:42:39.063Z
  */
 
 public·class·Solution·{
@@ -16,7 +16,8 @@ public·class·Solution·{
 ······for(int·i=0;i<nums.length;i++){
 ··········if(map.containsKey(nums[i])){
 ······}
-··············int·prevIdx=map.get
+··············int·prevIdx=map.get(nums[i]);
 ··········}
 ····}
+··············if(i-prevIdx·>=k)return·fal
 }
