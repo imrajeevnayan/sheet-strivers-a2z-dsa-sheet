147bd32e2bfbb96fd5064f0b65c8017a767a547c
CodeSync <codesync@users.noreply.github.com>
2026-06-07 16:25:11 +0530

    Add LintCode solution: Course

--- CodeSync/LintCode/unknown/all/Course/solution.py
+++ CodeSync/LintCode/unknown/all/Course/solution.py
@@ -1,25 +1,22 @@
 # Platform: LintCode
 # Problem: Course
-# URL: https://www.lintcode.com/problem/363/description
+# URL: https://www.lintcode.com/problem/380/description
 # Language: Python
 # Difficulty: Unknown
-# Topics: Opposite Direction Two Pointers, Stack, Two Pointers, Monotonic Stack, Apple, Twitter, Airbnb, Amazon
+# Topics: Linked List, Bloomberg Airbnb Amazon Microsoft, Bloomberg, Airbnb, Amazon, Microsoft, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-02T07:14:26.985Z
+# Synced: 2026-06-07T10:54:54.485Z
 
 public·class·Solution·{
-····public·int·trapRainWater(int[]·height)·{
-········if·(height·==·null·||·height.length·==·0)·return·0;
-····
-····int·left·=·0,·right·=·height.length·-·1;
-····int·leftMax·=·0,·rightMax·=·0;
-····int·water·=·0;
-····
-····while·(left·<·right)·{
-········if·(height[left]·<·height[right])·{
-············if·(height[left]·>=·leftMax)·{
-················leftMax·=·height[left];
-············}·else·{
-················water·+=·leftMax·-·height[left];
-············}
+····public·ListNode·getIntersectionNode(ListNode·headA,·ListNode·headB)·{
+·······if(headA==null·||·headB==null)return·null;
+·······ListNode·a=headA;
+·······ListNode·b=headB;
+·······while(a·!=b){
+···········a=(a==null)?·headB:a.next;
+···········b=(b==null)?headA:b.next;
+·······}
+·······return·b;
+····}
+}
