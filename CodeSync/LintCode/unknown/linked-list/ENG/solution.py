44329134829668ed3af57df84822e669f681f071
CodeSync <codesync@users.noreply.github.com>
2026-06-07 16:15:11 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.py
@@ -0,0 +1,32 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/380/
+# Language: Python
+# Difficulty: Unknown
+# Topics: Linked List, Bloomberg Airbnb Amazon Microsoft, Bloomberg, Airbnb, Amazon, Microsoft, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-06-07T10:45:09.893Z
+
+/**
+·*·Definition·for·ListNode
+·*·public·class·ListNode·{
+·*·····int·val;
+·*·····ListNode·next;
+·*·····ListNode(int·x)·{
+·*·········val·=·x;
+·*·········next·=·null;
+·*·····}
+·*·}
+·*/
+
+public·class·Solution·{
+····/**
+·····*·@param·headA:·the·first·list
+·····*·@param·headB:·the·second·list
+·····*·@return:·a·ListNode
+·····*/
+····public·ListNode·getIntersectionNode(ListNode·headA,·ListNode·headB)·{
+········//·write·your·code·here
+····}
+}
