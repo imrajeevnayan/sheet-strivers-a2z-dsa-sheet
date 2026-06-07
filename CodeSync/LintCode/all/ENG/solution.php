4b2fc5b553479fd5d661ddbe1629005102ea1e1b
CodeSync <codesync@users.noreply.github.com>
2026-06-07 23:17:19 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/all/ENG/solution.php
@@ -0,0 +1,19 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/3189/
+ * Language: PHP
+ * Difficulty: Unknown
+ * Topics: Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-07T17:47:07.859Z
+ */
+
+<?php
+require·"Father.php";
+//·write·your·code·here·
+class·Son{
+→   
+}
+?>
