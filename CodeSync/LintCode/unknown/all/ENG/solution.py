76b7643e1049219100f6acdcd7305ae00588640f
CodeSync <codesync@users.noreply.github.com>
2026-06-07 16:18:02 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/all/ENG/solution.py
+++ CodeSync/LintCode/unknown/all/ENG/solution.py
@@ -6,7 +6,7 @@
 # Topics: Linked List, Bloomberg Airbnb Amazon Microsoft, Bloomberg, Airbnb, Amazon, Microsoft, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-07T10:47:30.689Z
+# Synced: 2026-06-07T10:47:45.422Z
 
 public·class·Solution·{
 ····public·ListNode·getIntersectionNode(ListNode·headA,·ListNode·headB)·{
@@ -17,5 +17,6 @@ public·class·Solution·{
 ···········a=(a==null)headB:a.next;
 ·······}
 ····}
-···········b=(b==null)headA:b.next
+···········b=(b==null)headA:b.next;
+·······return·b;
 }
