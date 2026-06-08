5487fab0b4007c4ed4d02833684b16e79c779d0a
CodeSync <codesync@users.noreply.github.com>
2026-06-09 00:16:46 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/test-data-commit-output/ENG/solution.java
+++ CodeSync/LintCode/unknown/test-data-commit-output/ENG/solution.java
@@ -7,26 +7,21 @@
  * Topics: Stack, Test Data Commit Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-08T18:45:56.931Z
+ * Synced: 2026-06-08T18:46:38.415Z
  */
 
 ············else·{
-················int·v·=·stack.pop();
-················int·top·=·stack.pop();
+················int·v·=·st.pop();
+················int·top·=·st.pop();
 
 ················if·(v·==·0)·st.push(top·+·1);
 ············if·(c·==·'(')·st.push(0);
 
 ········for·(char·c·:·s.toCharArray())·{
-········stack.push(0);
+········st.push(0);
 ········Stack<Integer>·st=·new·Stack<>();
 ····public·int·parenthesesScore(String·s)·{
-public·class·Solution·{
-················else·stack.push(top·+·2·*·v);
-················
+················else·st.push(top·+·2·*·v);
 ············}
 ········}
-
-········return·stack.pop();
-····}
-}
+public·class·Solution·{
