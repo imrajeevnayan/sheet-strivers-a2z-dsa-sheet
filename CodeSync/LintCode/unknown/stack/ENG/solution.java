2d53697047478c92a599ec6aa36b02e2cfffe158
CodeSync <codesync@users.noreply.github.com>
2026-06-09 00:14:23 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stack/ENG/solution.java
+++ CodeSync/LintCode/unknown/stack/ENG/solution.java
@@ -7,14 +7,10 @@
  * Topics: Stack, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-08T18:44:13.202Z
+ * Synced: 2026-06-08T18:44:22.046Z
  */
 
 public·class·Solution·{
-····/**
-·····*·@param·s:·a·string·with·"("·and·")"
-·····*·@return:·return·the·score·of·the·string
-·····*/
 ····public·int·parenthesesScore(String·s)·{
 ········//·write·your·code·here
 ····}
