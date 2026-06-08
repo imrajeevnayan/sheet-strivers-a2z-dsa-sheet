96b8793fe5f83e71a3e9ccd9f8f43dfa46b5eb5d
CodeSync <codesync@users.noreply.github.com>
2026-06-09 00:14:52 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/test-data-commit-output/ENG/solution.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/268/
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Stack, Test Data Commit Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-08T18:44:45.049Z
+ */
+
+············}·else·{
+················int·v·=·stack.pop();
+················int·top·=·stack.pop();
+
+················if·(v·==·0)·{
+····················stack.push(top·+·1);
+················}·else·{
+····················stack.push(top·+·2·*·v);
+················}
+············}
+········}
+
+········return·stack.pop();
+····}
+}
