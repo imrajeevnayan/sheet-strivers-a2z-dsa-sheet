cd7b5f4286243c03a249173e167ada4c26a57cc9
CodeSync <codesync@users.noreply.github.com>
2026-06-09 00:16:26 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/test-data-test-output/ENG/solution.java
+++ CodeSync/LintCode/unknown/test-data-test-output/ENG/solution.java
@@ -7,15 +7,21 @@
  * Topics: Stack, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-08T18:44:13.202Z
+ * Synced: 2026-06-08T18:46:19.317Z
  */
 
-public·class·Solution·{
-····/**
-·····*·@param·s:·a·string·with·"("·and·")"
-·····*·@return:·return·the·score·of·the·string
-·····*/
+············else·{
+················int·v·=·stack.pop();
+················int·top·=·stack.pop();
+
+················if·(v·==·0)·st.push(top·+·1);
+············if·(c·==·'(')·st.push(0);
+
+········for·(char·c·:·s.toCharArray())·{
+········stack.push(0);
+········Stack<Integer>·st=·new·Stack<>();
 ····public·int·parenthesesScore(String·s)·{
-········//·write·your·code·here
-····}
-}
+················else·st.push(top·+·2·*·v);
+············}
+········}
+public·class·Solution·{
