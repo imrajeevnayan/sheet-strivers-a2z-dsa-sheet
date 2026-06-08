4b5780ad09a45ad82cb2ac7498da6f37c167d4d8
CodeSync <codesync@users.noreply.github.com>
2026-06-08 17:26:47 +0530

    Add GeeksforGeeks solution: Simple and Easy JAVA Solution:

---
+++ CodeSync/GeeksforGeeks/unknown/company-tag/Simple and Easy JAVA Solution/solution.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem: Simple and Easy JAVA Solution:
+ * URL: https://www.geeksforgeeks.org/problems/binary-to-decimal-1611122504--161115/1
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Expected Complexities, Company Tags Adobe, Company Tags, Adobe, Topic Tags, CPP, Related Articles, Program Binary Decimal Conversion
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-08T11:56:33.655Z
+ */
+
+class Solution {
+    public int binaryToDecimal(String b) {
+        return Integer.parseInt(b, 2);
+    }
+}
