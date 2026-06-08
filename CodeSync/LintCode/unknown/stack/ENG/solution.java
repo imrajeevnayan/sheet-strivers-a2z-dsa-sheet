268a22fdee723c6147007685a13a69cc6dcbd3cd
CodeSync <codesync@users.noreply.github.com>
2026-06-09 00:14:04 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stack/ENG/solution.java
+++ CodeSync/LintCode/unknown/stack/ENG/solution.java
@@ -7,10 +7,29 @@
  * Topics: Stack, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-08T18:44:00.610Z
+ * Synced: 2026-06-08T18:44:02.797Z
  */
 
-Example 1:
+class·Solution·{
+····public·int·scoreOfParentheses(String·s)·{
+········Stack<Integer>·stack·=·new·Stack<>();
+········stack.push(0);
 
-Input: "()"
-Output: 1
+········for·(char·c·:·s.toCharArray())·{
+············if·(c·==·'(')·{
+················stack.push(0);
+············}·else·{
+················int·v·=·stack.pop();
+················int·top·=·stack.pop();
+
+················if·(v·==·0)·{
+····················stack.push(top·+·1);
+················}·else·{
+····················stack.push(top·+·2·*·v);
+················}
+············}
+········}
+
+········return·stack.pop();
+····}
+}
