842c5827e80ae6b30edb350b1a6d078bd088fd22
CodeSync <codesync@users.noreply.github.com>
2026-06-09 22:15:31 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stack/ENG/solution.java
+++ CodeSync/LintCode/unknown/stack/ENG/solution.java
@@ -4,24 +4,13 @@
  * URL: https://www.lintcode.com/problem/268/
  * Language: Java
  * Difficulty: Unknown
- * Topics: Stack, Test Data Commit Output
+ * Topics: Stack, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-08T18:46:38.415Z
+ * Synced: 2026-06-09T16:45:28.990Z
  */
 
-············else·{
-················int·v·=·st.pop();
-················int·top·=·st.pop();
+Example 1:
 
-················if·(v·==·0)·st.push(top·+·1);
-············if·(c·==·'(')·st.push(0);
-
-········for·(char·c·:·s.toCharArray())·{
-········st.push(0);
-········Stack<Integer>·st=·new·Stack<>();
-····public·int·parenthesesScore(String·s)·{
-················else·st.push(top·+·2·*·v);
-············}
-········}
-public·class·Solution·{
+Input: "()"
+Output: 1
