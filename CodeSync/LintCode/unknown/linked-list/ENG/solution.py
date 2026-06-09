c731f976f6b389ff8cd4f842fdb202eb24a035ea
CodeSync <codesync@users.noreply.github.com>
2026-06-09 12:15:09 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/linked-list/ENG/solution.py
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.py
@@ -1,22 +1,23 @@
 # Platform: LintCode
 # Problem: ENG
-# URL: https://www.lintcode.com/problem/380/
+# URL: https://www.lintcode.com/problem/35/
 # Language: Python
 # Difficulty: Unknown
-# Topics: Linked List, Bloomberg Airbnb Amazon Microsoft, Bloomberg, Airbnb, Amazon, Microsoft, Test Data Test Output
+# Topics: Linked List, ByteDance, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-07T10:49:01.363Z
+# Synced: 2026-06-09T06:45:07.598Z
 
 public·class·Solution·{
-····public·ListNode·getIntersectionNode(ListNode·headA,·ListNode·headB)·{
-·······if(headA==null·||·headB==null)return·null;
-·······ListNode·a=headA;
-·······ListNode·b=headB;
-·······while(a·!=b){
-···········a=(a==null)?·headB:a.next;
-·······}
+····public·ListNode·reverse(ListNode·head)·{
+·····ListNode·prev=null;
+·····ListNode·curr=head;
+·····while(curr!=null){
+·········ListNode·temp=curr.next;
+·········curr.next=prev;
+·········prev=curr;
+·········curr=temp;
+·····}
+·····return·prev;
 ····}
-···········b=(b==null)?headA:b.next;
-·······return·b;
 }
