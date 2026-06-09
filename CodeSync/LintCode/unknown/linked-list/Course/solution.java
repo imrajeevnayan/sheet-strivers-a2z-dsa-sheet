cda8b9b5971d8203fb1656467788aaf7e8f57be1
CodeSync <codesync@users.noreply.github.com>
2026-06-09 16:42:17 +0530

    Add LintCode solution: Course

--- CodeSync/LintCode/unknown/linked-list/Course/solution.java
+++ CodeSync/LintCode/unknown/linked-list/Course/solution.java
@@ -1,24 +1,33 @@
 /*
  * Platform: LintCode
  * Problem: Course
- * URL: https://www.lintcode.com/problem/380/description
+ * URL: https://www.lintcode.com/problem/223/description
  * Language: Java
  * Difficulty: Unknown
- * Topics: Linked List, Bloomberg Airbnb Amazon Microsoft, Bloomberg, Airbnb, Amazon, Microsoft, Test Data Test Output, Dynamic Programming
+ * Topics: Linked List, Facebook IXL Amazon, Facebook, IXL, Amazon, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-07T17:46:44.159Z
+ * Synced: 2026-06-09T11:12:16.151Z
  */
 
+/**
+·*·Definition·for·ListNode
+·*·public·class·ListNode·{
+·*·····int·val;
+·*·····ListNode·next;
+·*·····ListNode(int·x)·{
+·*·········val·=·x;
+·*·········next·=·null;
+·*·····}
+·*·}
+·*/
+
 public·class·Solution·{
-····public·ListNode·getIntersectionNode(ListNode·headA,·ListNode·headB)·{
-·······if(headA==null·||·headB==null)return·null;
-·······ListNode·a=headA;
-·······ListNode·b=headB;
-·······while(a·!=b){
-···········a=(a==null)?·headB:a.next;
-···········b=(b==null)?headA:b.next;
-·······}
-·······return·b;
+····/**
+·····*·@param·head:·A·ListNode.
+·····*·@return:·A·boolean.
+·····*/
+····public·boolean·isPalindrome(ListNode·head)·{
+········//·write·your·code·here
 ····}
 }
