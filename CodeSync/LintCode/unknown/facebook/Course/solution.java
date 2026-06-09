73679da065d88190a7d5dac7c0ffa75a0b0c28a6
CodeSync <codesync@users.noreply.github.com>
2026-06-09 17:10:38 +0530

    Add LintCode solution: Course

---
+++ CodeSync/LintCode/unknown/facebook/Course/solution.java
@@ -0,0 +1,17 @@
+/*
+ * Platform: LintCode
+ * Problem: Course
+ * URL: https://www.lintcode.com/problem/223/description
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Linked List, Facebook IXL Amazon, Facebook, IXL, Amazon, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-09T11:40:25.135Z
+ */
+
+public·class·Solution·{
+····public·boolean·isPalindrome(ListNode·head)·{
+····}
+········
+}
