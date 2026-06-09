c463133f75a6601d032043e4d6e8c3d9d81642ca
CodeSync <codesync@users.noreply.github.com>
2026-06-09 12:15:39 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/all/ENG/solution.py
+++ CodeSync/LintCode/all/ENG/solution.py
@@ -1,21 +1,23 @@
 # Platform: LintCode
 # Problem: ENG
-# URL: https://www.lintcode.com/problem/209/
+# URL: https://www.lintcode.com/problem/35/
 # Language: Python
-# Difficulty: Unknown
-# Topics: Hash TableString, Hash Table, String, Bloomberg Amazon Microsoft Google, Bloomberg, Amazon, Microsoft, Google
+# Difficulty: Medium
+# Topics: Linked List, ByteDance, Test Data Commit Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-07T18:04:32.874Z
+# Synced: 2026-06-09T06:45:14.190Z
 
 public·class·Solution·{
-··public·char·firstUniqChar(String·str)·{
-···Map<Character,Integer>map=new·HashMap<>();
-···for(char·c:str.toCharArray())map.put(c,map.getOrDefault(c,0)+1);
-···for·(int·i·=·0;·i<str.length();·i++)·{
-·······char·c·=str.charAt(i);
-········if·(map.get(c)·==·1)·return·c;·
-····}
-···return·'·';
+····public·ListNode·reverse(ListNode·head)·{
+·····ListNode·prev=null;
+·····ListNode·curr=head;
+·····while(curr!=null){
+·········ListNode·temp=curr.next;
+·········curr.next=prev;
+·········prev=curr;
+·········curr=temp;
+·····}
+·····return·prev;
 ····}
 }
