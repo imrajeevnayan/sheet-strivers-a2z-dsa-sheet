004b980c4b484f40a36588ec3c084aadb7ebd9ad
CodeSync <codesync@users.noreply.github.com>
2026-06-09 16:40:45 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/easy/linked-list/ENG/solution.java
+++ CodeSync/LintCode/easy/linked-list/ENG/solution.java
@@ -1,27 +1,19 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/36/
+ * URL: https://www.lintcode.com/problem/372/
  * Language: Java
  * Difficulty: Easy
- * Topics: Linked List, Test Data Commit Output
+ * Topics: Linked List, Apple Adobe Microsoft, Apple, Adobe, Microsoft, 174 Remove Nth Node From End of List Easy, Test Data Commit Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T06:49:30.196Z
+ * Synced: 2026-06-09T11:10:44.238Z
  */
 
-········ListNode·prev·=·dummy;
-········for·(int·i·=·1;·i·<·m;·i++)·{
-············prev·=·prev.next;
-········}
-········ListNode·curr·=·prev.next;
-········for·(int·i·=·0;·i·<·n·-·m;·i++)·{
-············ListNode·next·=·curr.next;·····
-·············curr.next·=·next.next;
-
-········dummy.next·=·head;
-
-········ListNode·dummy·=·new·ListNode(0);
-public·class·Solution·{
-····public·ListNode·reverseBetween(ListNode·head,·int·m,·int·n)·{
-········if·(head·==·null·||·m·==·n)·return·head;
+class·Solution·{
+····public·void·deleteNode(ListNode·node)·{
+········if(node==null·||·node.next==null)·return;
+········node.val=node.next.val;
+········node.next=node.next.next;
+····}
+}
