2d2e89a3b03d8dfb5ddf469f84855af82616b25c
CodeSync <codesync@users.noreply.github.com>
2026-06-09 13:36:43 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/linked-list/ENG/solution.py
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.py
@@ -6,18 +6,6 @@
 # Topics: Linked List, ByteDance, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-09T06:45:07.598Z
+# Synced: 2026-06-09T08:06:42.175Z
 
-public·class·Solution·{
-····public·ListNode·reverse(ListNode·head)·{
-·····ListNode·prev=null;
-·····ListNode·curr=head;
-·····while(curr!=null){
-·········ListNode·temp=curr.next;
-·········curr.next=prev;
-·········prev=curr;
-·········curr=temp;
-·····}
-·····return·prev;
-····}
-}
+linked list = 1->2->3->null
