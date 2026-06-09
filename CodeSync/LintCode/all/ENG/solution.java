958f4d228f1d9e4737206ec13495c82a4051c9b1
CodeSync <codesync@users.noreply.github.com>
2026-06-09 16:37:37 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/all/ENG/solution.java
+++ CodeSync/LintCode/all/ENG/solution.java
@@ -1,33 +1,18 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/36/
+ * URL: https://www.lintcode.com/problem/372/
  * Language: Java
  * Difficulty: Unknown
- * Topics: Linked List, Test Data Test Output
+ * Topics: Linked List, Apple Adobe Microsoft, Apple, Adobe, Microsoft, 174 Remove Nth Node From End of List Easy, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T09:32:11.769Z
+ * Synced: 2026-06-09T11:07:14.022Z
  */
 
 public·class·Solution·{
-····public·ListNode·reverseBetween(ListNode·head,·int·m,·int·n)·{
-········if·(head·==·null·||·m·==·n)·return·head;
-
-········ListNode·dummy·=·new·ListNode(0);*
-········dummy.next·=·head;
-
-········ListNode·prev·=·dummy;
-········for·(int·i·=·1;·i·<·m;·i++)·{
-············prev·=·prev.next;
-········}
-········ListNode·curr·=·prev.next;
-········for·(int·i·=·0;·i·<·n·-·m;·i++)·{
-············ListNode·next·=·curr.next;·····
-············curr.next·=·next.next;
-············next.next·=·prev.next;
-············prev.next·=·next;
-········}
-········return·dummy.next;
+····public·void·deleteNode(ListNode·node)·{
+········node.val=node.next.val;
 ····}
+········node.next=node.next.next
 }
