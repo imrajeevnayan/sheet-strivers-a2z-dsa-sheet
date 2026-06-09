1dd70928d63d5a6d283f65799d7f12c61dd1d9a5
CodeSync <codesync@users.noreply.github.com>
2026-06-09 23:53:46 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/all/ENG/solution.java
+++ CodeSync/LintCode/all/ENG/solution.java
@@ -1,16 +1,27 @@
 /*
  * Platform: LintCode
  * Problem: ENG
- * URL: https://www.lintcode.com/problem/268/
+ * URL: https://www.lintcode.com/problem/65/
  * Language: Java
  * Difficulty: Unknown
- * Topics: Stack, Test Data Test Output
+ * Topics: Binary Search, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T16:45:28.990Z
+ * Synced: 2026-06-09T18:23:24.735Z
  */
 
-Example 1:
-
-Input: "()"
-Output: 1
+public·class·Solution·{
+····public·double·findMedianSortedArrays(int[]·a,·int[]·b)·{
+········int·n1=a.length,n2=b.length;
+········int·merge[]=new·int[n1+n2];
+········int·i=0,j=0,k=0;
+········while(i·<·n1·&&·j<·n2){
+············if(a[i]<·b[j])merge[k++]=a[i++];
+············else·merge[k++]=b[j++];
+········}
+········while(i<·n1)merge[k++]=a[i++];
+········while(j<n2)merge[k++]=b[j++];
+········int·n=merge.length;
+········if(n%2==1)return·merge[n/2];
+········else·return(merge[n/2]+merge[(n/2)-1])/2.0;
+····}
