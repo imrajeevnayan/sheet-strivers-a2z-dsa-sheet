368eee36b3ef4e65fe6facb58509cffb1df0aa4e
CodeSync <codesync@users.noreply.github.com>
2026-06-09 17:51:54 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/stack/ENG/solution.py
+++ CodeSync/LintCode/unknown/stack/ENG/solution.py
@@ -3,12 +3,27 @@
 # URL: https://www.lintcode.com/problem/268/
 # Language: Python
 # Difficulty: Unknown
-# Topics: Stack
+# Topics: Stack, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-09T12:21:49.612Z
+# Synced: 2026-06-09T12:21:52.695Z
 
-Example 1:
+public·class·Solution·{
+····public·int·parenthesesScore(String·s)·{
+········Stack<Integer>·st=·new·Stack<>();
+········st.push(0);
 
-Input: "()"
-Output: 1
+········for·(char·c·:·s.toCharArray())·{
+············if·(c·==·'(')·st.push(0);
+············else·{
+················int·v·=·st.pop();
+················int·top·=·st.pop();
+
+················if·(v·==·0)·st.push(top·+·1);
+················else·st.push(top·+·2·*·v);
+············}
+········}
+
+········return·st.pop();
+····}
+}
