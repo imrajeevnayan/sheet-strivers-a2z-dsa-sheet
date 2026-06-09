7b62ff499fec1c11a4903342fdcd2fe0fedad124
CodeSync <codesync@users.noreply.github.com>
2026-06-09 17:19:14 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/linked-list/ENG/solution.py
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.py
@@ -3,14 +3,14 @@
 # URL: https://www.lintcode.com/problem/228/
 # Language: Python
 # Difficulty: Unknown
-# Topics: Same Direction Two Pointers, Linked List, Two Pointers, 174 Remove Nth Node From End of List Easy, Test Data Commit Output
+# Topics: Same Direction Two Pointers, Linked List, Two Pointers, 174 Remove Nth Node From End of List Easy, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-09T11:48:57.317Z
+# Synced: 2026-06-09T11:49:07.202Z
 
 public·class·Solution·{
 ····public·ListNode·middleNode(ListNode·head)·{
-········ListNode·slow=head,fast=head;
+········ListNode·slow=head,fast=head.next;
 ········while(fast!=null·&&·fast.next!=null){
 ············fast=fast.next.next;
 ········}
