1beae652e7cd03c249fdd3dc453c74b040c4fee8
CodeSync <codesync@users.noreply.github.com>
2026-06-09 16:42:14 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/facebook-ixl-amazon/ENG/solution.java
@@ -0,0 +1,33 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/223/
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Linked List, Facebook IXL Amazon, Facebook, IXL, Amazon, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-09T11:12:06.261Z
+ */
+
+/**
+·*·Definition·for·ListNode
+·*·public·class·ListNode·{
+·*·····int·val;
+·*·····ListNode·next;
+·*·····ListNode(int·x)·{
+·*·········val·=·x;
+·*·········next·=·null;
+·*·····}
+·*·}
+·*/
+
+public·class·Solution·{
+····/**
+·····*·@param·head:·A·ListNode.
+·····*·@return:·A·boolean.
+·····*/
+····public·boolean·isPalindrome(ListNode·head)·{
+········//·write·your·code·here
+····}
+}
