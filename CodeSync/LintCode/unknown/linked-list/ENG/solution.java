8c8010a65d0cb631a3f0f1cc4e1d7dac4bcd9e44
CodeSync <codesync@users.noreply.github.com>
2026-06-09 12:19:13 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/linked-list/ENG/solution.java
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.java
@@ -7,11 +7,8 @@
  * Topics: Linked List, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T06:49:01.662Z
+ * Synced: 2026-06-09T06:49:12.779Z
  */
-
-········ListNode·dummy·=·new·ListNode(0);
-········dummy.next·=·head;
 
 ········ListNode·prev·=·dummy;
 ········for·(int·i·=·1;·i·<·m;·i++)·{
@@ -19,9 +16,13 @@
 ········}
 ········ListNode·curr·=·prev.next;
 ········for·(int·i·=·0;·i·<·n·-·m;·i++)·{
-············ListNode·next·=·curr.next;······//·The·node·to·be·moved
-············
-············//·Step·A:·Remove·'next'·from·its·current·position
-············curr.next·=·next.next;
+············ListNode·next·=·curr.next;·····
+·············curr.next·=·next.next;
 ············
 ············//·Step·B:·Insert·'next'·at·the·beginning·of·the·reversed·section·(after·'prev')
+············next.next·=·prev.next;
+············prev.next·=·next;
+········}
+
+········return·dummy.next;
+····}
