5474d878b7fe97f2c46c7cbe882163c4abc75b48
CodeSync <codesync@users.noreply.github.com>
2026-06-09 12:19:25 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -7,21 +7,22 @@
  * Topics: Linked List, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T06:48:55.485Z
+ * Synced: 2026-06-09T06:49:12.779Z
  */
-
-········ListNode·dummy·=·new·ListNode(0);
-
-········if·(head·==·null·||·m·==·n)·return·head;
-····public·ListNode·reverseBetween(ListNode·head,·int·m,·int·n)·{
-public·class·Solution·{
-········dummy.next·=·head;
 
 ········ListNode·prev·=·dummy;
 ········for·(int·i·=·1;·i·<·m;·i++)·{
 ············prev·=·prev.next;
 ········}
 ········ListNode·curr·=·prev.next;
-
-········//·2.·Perform·(n·-·m)·iterations·to·move·nodes·to·the·front·of·the·reversed·section
 ········for·(int·i·=·0;·i·<·n·-·m;·i++)·{
+············ListNode·next·=·curr.next;·····
+·············curr.next·=·next.next;
+············
+············//·Step·B:·Insert·'next'·at·the·beginning·of·the·reversed·section·(after·'prev')
+············next.next·=·prev.next;
+············prev.next·=·next;
+········}
+
+········return·dummy.next;
+····}
