5faa06337a4016f8e5eb572b8af36d562545985c
CodeSync <codesync@users.noreply.github.com>
2026-06-09 17:19:05 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/two-pointer/ENG/solution.py
+++ CodeSync/LintCode/unknown/two-pointer/ENG/solution.py
@@ -6,11 +6,11 @@
 # Topics: Same Direction Two Pointers, Linked List, Two Pointers, 174 Remove Nth Node From End of List Easy, Test Data Commit Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-09T11:49:02.520Z
+# Synced: 2026-06-09T11:49:04.030Z
 
 public·class·Solution·{
 ····public·ListNode·middleNode(ListNode·head)·{
-········ListNode·slow=head,fast=head.nex;
+········ListNode·slow=head,fast=head.next;
 ········while(fast!=null·&&·fast.next!=null){
 ············fast=fast.next.next;
 ········}
