7f202ed9cd4a85852bc6482094b9fe06570940c6
CodeSync <codesync@users.noreply.github.com>
2026-06-09 17:15:10 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/two-pointer/ENG/solution.py
@@ -0,0 +1,31 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/228/
+# Language: Python
+# Difficulty: Unknown
+# Topics: Same Direction Two Pointers, Linked List, Two Pointers, 174 Remove Nth Node From End of List Easy, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-06-09T11:45:09.111Z
+
+/**
+·*·Definition·for·ListNode
+·*·public·class·ListNode·{
+·*·····int·val;
+·*·····ListNode·next;
+·*·····ListNode(int·x)·{
+·*·········val·=·x;
+·*·········next·=·null;
+·*·····}
+·*·}
+·*/
+
+public·class·Solution·{
+····/**
+·····*·@param·head:·the·head·of·linked·list.
+·····*·@return:·a·middle·node·of·the·linked·list
+·····*/
+····public·ListNode·middleNode(ListNode·head)·{
+········//·write·your·code·here
+····}
+}
