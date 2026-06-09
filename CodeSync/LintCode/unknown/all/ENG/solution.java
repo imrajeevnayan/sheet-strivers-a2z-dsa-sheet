2ed6b63e68b4d0104833810762e2f77c7820c260
CodeSync <codesync@users.noreply.github.com>
2026-06-09 12:17:54 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/all/ENG/solution.java
+++ CodeSync/LintCode/unknown/all/ENG/solution.java
@@ -7,7 +7,7 @@
  * Topics: Linked List, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T06:47:17.048Z
+ * Synced: 2026-06-09T06:47:41.993Z
  */
 
 public·class·Solution·{
@@ -24,10 +24,4 @@ public·class·Solution·{
 ········for·(int·i·=·0;·i·<·right·-·left;·i++)·{
 ············Node·next·=·curr.next;······
 ············curr.next·=·next.next;······
-············next.next·=·prev.next;······//·Point·'next'·to·the·start·of·reversed·part
-············prev.next·=·next;···········//·Attach·'next'·to·'prev'
-········}
-
-········return·dummy.next;
-····}
-}
+············next.next·=·prev.next;·····
