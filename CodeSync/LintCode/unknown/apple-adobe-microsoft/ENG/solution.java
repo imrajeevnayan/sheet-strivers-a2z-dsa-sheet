36f13364ed69586383fc3bebe0ef60d4a692915f
CodeSync <codesync@users.noreply.github.com>
2026-06-09 16:40:36 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/apple-adobe-microsoft/ENG/solution.java
+++ CodeSync/LintCode/unknown/apple-adobe-microsoft/ENG/solution.java
@@ -7,12 +7,13 @@
  * Topics: Linked List, Apple Adobe Microsoft, Apple, Adobe, Microsoft, 174 Remove Nth Node From End of List Easy, Test Data Commit Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T11:09:22.854Z
+ * Synced: 2026-06-09T11:10:30.089Z
  */
 
 class·Solution·{
 ····public·void·deleteNode(ListNode·node)·{
-····}
-········node.next=node.next.next;
+········if(node==null·||·node.next==null)
 ········node.val=node.next.val;
+········node.next=node.next.next;
+····}
 }
