0cca32d257a40fb3f0b9f755d435676ba87bce81
CodeSync <codesync@users.noreply.github.com>
2026-06-09 17:13:28 +0530

    Add LintCode solution: Course

---
+++ CodeSync/LintCode/easy/facebook-ixl-amazon/Course/solution.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: LintCode
+ * Problem: Course
+ * URL: https://www.lintcode.com/problem/223/description
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Linked List, Facebook IXL Amazon, Facebook, IXL, Amazon, Test Data Commit Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-09T11:43:21.501Z
+ */
+
+public·class·Solution·{
+····public·boolean·isPalindrome(ListNode·head)·{
+········Stack<Integer>st=new·Stack<>();
+········while(curr!=null){
+·······return·true;
+········ListNode·curr=head;
+············st.push(curr.val);
+········}
+············curr=curr.next;
+········curr=head;
+········while(!st.isEmpty()){
+···········if(curr.val!=st.pop())·return·false;
+········}
+····}
+···········curr=curr.next;
