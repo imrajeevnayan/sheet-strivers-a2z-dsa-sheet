fb7ff6a66e568b1c2a325a7a0a6fa69f87101f18
CodeSync <codesync@users.noreply.github.com>
2026-06-09 12:17:18 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/linked-list/ENG/solution.java
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.java
@@ -7,7 +7,7 @@
  * Topics: Linked List, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-09T06:47:13.947Z
+ * Synced: 2026-06-09T06:47:17.048Z
  */
 
 public·class·Solution·{
