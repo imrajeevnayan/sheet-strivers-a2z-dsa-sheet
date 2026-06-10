f34c0f341fb6e0a074243ef01ba81a13c8fbaeb2
CodeSync <codesync@users.noreply.github.com>
2026-06-10 22:52:31 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/array/ENG/solution.java
+++ CodeSync/LintCode/unknown/array/ENG/solution.java
@@ -7,20 +7,21 @@
  * Topics: Prefix Sum ArrayArray, Prefix Sum Array, Array, OpenAI, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-10T17:21:13.948Z
+ * Synced: 2026-06-10T17:22:18.179Z
  */
 
-class·NumArray·{
-······int·ans[];
+········for·(int·i·=·1;·i·<·nums.length;·i++)·{
+············ans[i]·=·ans[i·-·1]·+·nums[i];
+
 ····public·NumArray(int[]·nums)·{
-····}
+········ans·=·new·int[nums.length];
+
+········ans[0]·=·nums[0];
+
 ····public·int·sumRange(int·i,·int·j)·{
-····}
-········int·ans[]=new·int[nums.length];
-········ans[0]=nums[0];
-········for(int·i=1;i<nums.length;i++){
-············ans[i]=ans[i-1]+nums[i];
 ········}
+····}
+········if·(i·==·0)·return·ans[j];
+········return·ans[j]·-·ans[i·-·1];
+····}
 }
-········if(i==0)return·ans[j];
-········return·ans[j]-ans[i-1];
