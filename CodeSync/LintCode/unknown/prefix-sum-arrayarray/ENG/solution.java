8ade968efa2e10dc24f4490d3c8990f47673b2a3
CodeSync <codesync@users.noreply.github.com>
2026-06-10 22:52:48 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/prefix-sum-arrayarray/ENG/solution.java
+++ CodeSync/LintCode/unknown/prefix-sum-arrayarray/ENG/solution.java
@@ -4,24 +4,23 @@
  * URL: https://www.lintcode.com/problem/943/
  * Language: Java
  * Difficulty: Unknown
- * Topics: Prefix Sum ArrayArray, Prefix Sum Array, Array, OpenAI, Test Data Test Output
+ * Topics: Prefix Sum ArrayArray, Prefix Sum Array, Array, OpenAI, Test Data Commit Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-10T17:22:25.229Z
+ * Synced: 2026-06-10T17:22:47.265Z
  */
 
 ········for·(int·i·=·1;·i·<·nums.length;·i++)·{
 ············ans[i]·=·ans[i·-·1]·+·nums[i];
-
 ····public·NumArray(int[]·nums)·{
 ········ans·=·new·int[nums.length];
-
 ········ans[0]·=·nums[0];
-
 ····public·int·sumRange(int·i,·int·j)·{
 ········}
 ····}
 ········if·(i·==·0)·return·ans[j];
+class·NumArray·{
+····int[]·ans;
 ········return·ans[j]·-·ans[i·-·1];
 ····}
 }
