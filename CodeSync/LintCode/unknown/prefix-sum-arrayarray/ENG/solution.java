c5953629f3d665fd56b2eb14414c292073626cd1
CodeSync <codesync@users.noreply.github.com>
2026-06-10 22:46:34 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/prefix-sum-arrayarray/ENG/solution.java
+++ CodeSync/LintCode/unknown/prefix-sum-arrayarray/ENG/solution.java
@@ -7,11 +7,11 @@
  * Topics: Prefix Sum ArrayArray, Prefix Sum Array, Array, OpenAI, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-10T17:16:21.759Z
+ * Synced: 2026-06-10T17:16:33.227Z
  */
 
 class·NumArray·{
-
+······
 ····public·NumArray(int[]·nums)·{
 ········
 ····}
