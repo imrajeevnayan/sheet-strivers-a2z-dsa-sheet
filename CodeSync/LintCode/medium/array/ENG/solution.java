4292d6ac4a25c2deb7e7e3dd8a4a7c834ab23bd0
CodeSync <codesync@users.noreply.github.com>
2026-06-10 23:34:14 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/medium/array/ENG/solution.java
@@ -0,0 +1,26 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/943/
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Prefix Sum ArrayArray, Prefix Sum Array, Array, OpenAI, Test Data Commit Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-10T17:42:46.704Z
+ */
+
+········for·(int·i·=·1;·i·<·nums.length;·i++)·{
+············ans[i]·=·ans[i·-·1]·+·nums[i];
+····public·NumArray(int[]·nums)·{
+········ans·=·new·int[nums.length];
+········ans[0]·=·nums[0];
+····public·int·sumRange(int·i,·int·j)·{
+········}
+····}
+········if·(i·==·0)·return·ans[j];
+class·NumArray·{
+····int[]·ans;
+········return·ans[j]·-·ans[i·-·1];
+····}
+}
