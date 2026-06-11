a6c727a0bfeb602b612ecd99c6a2ef3fc739263c
CodeSync <codesync@users.noreply.github.com>
2026-06-11 12:20:31 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/linked-listsimulation/ENG/solution.java
@@ -0,0 +1,34 @@
+/*
+ * Platform: LintCode
+ * Problem: ENG
+ * URL: https://www.lintcode.com/problem/167/
+ * Language: Java
+ * Difficulty: Unknown
+ * Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-11T06:49:14.966Z
+ */
+
+/**
+·*·Definition·for·ListNode
+·*·public·class·ListNode·{
+·*·····int·val;
+·*·····ListNode·next;
+·*·····ListNode(int·x)·{
+·*·········val·=·x;
+·*·········next·=·null;
+·*·····}
+·*·}
+·*/
+
+public·class·Solution·{
+····/**
+·····*·@param·l1:·the·first·list
+·····*·@param·l2:·the·second·list
+·····*·@return:·the·sum·list·of·l1·and·l2·
+·····*/
+····public·ListNode·addLists(ListNode·l1,·ListNode·l2)·{
+········//·write·your·code·here
+····}
+}
