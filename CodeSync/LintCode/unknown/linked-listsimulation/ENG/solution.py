7bf71ae9fe21e42398e5f2fd92ef6c228e8faf7b
CodeSync <codesync@users.noreply.github.com>
2026-06-11 15:34:02 +0530

    Add LintCode solution: ENG

---
+++ CodeSync/LintCode/unknown/linked-listsimulation/ENG/solution.py
@@ -0,0 +1,15 @@
+# Platform: LintCode
+# Problem: ENG
+# URL: https://www.lintcode.com/problem/167/
+# Language: Python
+# Difficulty: Unknown
+# Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-06-11T10:03:59.565Z
+
+public·class·Solution·{
+····public·ListNode·addLists(ListNode·l1,·ListNode·l2)·{
+······
+····}
+}
