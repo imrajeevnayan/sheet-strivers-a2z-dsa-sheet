b716c379f2070f9bb85cb65f0abb2beff10a2677
CodeSync <codesync@users.noreply.github.com>
2026-06-11 15:34:09 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/linked-list/ENG/solution.py
+++ CodeSync/LintCode/unknown/linked-list/ENG/solution.py
@@ -1,21 +1,15 @@
 # Platform: LintCode
 # Problem: ENG
-# URL: https://www.lintcode.com/problem/228/
+# URL: https://www.lintcode.com/problem/167/
 # Language: Python
 # Difficulty: Unknown
-# Topics: Same Direction Two Pointers, Linked List, Two Pointers, 174 Remove Nth Node From End of List Easy, Test Data Test Output
+# Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-06-09T11:49:07.202Z
+# Synced: 2026-06-11T10:03:59.565Z
 
 public·class·Solution·{
-····public·ListNode·middleNode(ListNode·head)·{
-········ListNode·slow=head,fast=head.next;
-········while(fast!=null·&&·fast.next!=null){
-············fast=fast.next.next;
-········}
-············slow=slow.next;
-········if·(head·==·null)return·null;
-········return·slow;
+····public·ListNode·addLists(ListNode·l1,·ListNode·l2)·{
+······
 ····}
 }
