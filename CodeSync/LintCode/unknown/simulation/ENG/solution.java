3c3044ea598b071e27ee33e4785d4840aa8ade85
CodeSync <codesync@users.noreply.github.com>
2026-06-12 08:36:20 +0530

    Add LintCode solution: ENG

--- CodeSync/LintCode/unknown/simulation/ENG/solution.java
+++ CodeSync/LintCode/unknown/simulation/ENG/solution.java
@@ -7,28 +7,11 @@
  * Topics: Linked ListSimulation, Linked List, Simulation, Test Data Test Output
  * Runtime: N/A
  * Memory: N/A
- * Synced: 2026-06-11T06:49:14.966Z
+ * Synced: 2026-06-12T03:05:56.599Z
  */
 
-/**
-·*·Definition·for·ListNode
-·*·public·class·ListNode·{
-·*·····int·val;
-·*·····ListNode·next;
-·*·····ListNode(int·x)·{
-·*·········val·=·x;
-·*·········next·=·null;
-·*·····}
-·*·}
-·*/
-
 public·class·Solution·{
-····/**
-·····*·@param·l1:·the·first·list
-·····*·@param·l2:·the·second·list
-·····*·@return:·the·sum·list·of·l1·and·l2·
-·····*/
 ····public·ListNode·addLists(ListNode·l1,·ListNode·l2)·{
-········//·write·your·code·here
+······
 ····}
 }
