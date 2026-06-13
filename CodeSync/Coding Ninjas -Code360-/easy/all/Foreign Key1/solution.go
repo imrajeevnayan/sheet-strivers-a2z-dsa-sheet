adb613a008e62a3cde9d1dfedd2dba566a63bf32
CodeSync <codesync@users.noreply.github.com>
2026-06-13 22:06:37 +0530

    Add Coding Ninjas (Code360) solution: Foreign Key1

---
+++ CodeSync/Coding Ninjas -Code360-/easy/all/Foreign Key1/solution.go
@@ -0,0 +1,21 @@
+/*
+ * Platform: Coding Ninjas (Code360)
+ * Problem: Foreign Key1
+ * URL: https://www.naukri.com/code360/problem-of-the-day/mcq
+ * Language: Go
+ * Difficulty: Easy
+ * Topics: Uncategorized
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-06-13T16:36:32.918Z
+ */
+
+Table Client:
+ClientID (primary Key)
+clientname
+clientphone
+
+Table : Sales
+SalesID (Primary Key)
+ClientID
+Profit
